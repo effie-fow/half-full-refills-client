f155af720fe45a994d6a8db06246e0e3f40545a3
Effie Fowler <effiejofowler@gmail.com>
2024-09-28 15:50:32 +0100

    fix small bugs and spelling errors

--- src/components/AboutProducts/AboutProducts.jsx
+++ src/components/AboutProducts/AboutProducts.jsx
@@ -5,14 +5,8 @@ export const AboutProducts = () => {
     <section className="about-products">
       <h2 className="about-products__section-heading">Our Categories</h2>
       <p className="about-products__description">
-        In case you're in the process of nominating or browsing our listings and
-        would like to know more about our categories, we have listed what we
-        mean by the 18 categories currently available to nominate and browse by
-        below.
-      </p>
-      <p className="about-products__description">
-        We know some of our categories may seem are broad, but we have selected
-        them because we believe they capture most 'everyday items' and/or are
+        We know some of our categories may seem broad, but we have selected them
+        because we believe they capture most 'everyday items' and/or are
         important features for customers to know about.
       </p>
       <span className="about-products__reminder">Remember...</span>
